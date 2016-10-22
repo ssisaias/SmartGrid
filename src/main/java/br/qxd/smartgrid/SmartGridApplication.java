1c4f5f76a2caae1310bdf9d7988f2254b1f55428
brianviana <brianmarquesviana@gmail.com>
2016-10-22 18:04:53 -0300

    commit inicial do source

---
+++ src/main/java/br/qxd/smartgrid/SmartGridApplication.java
@@ -0,0 +1,12 @@
+package br.qxd.smartgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGridApplication.class, args);
+	}
+}
