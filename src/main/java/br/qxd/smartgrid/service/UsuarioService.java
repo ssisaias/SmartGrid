3a875f1ff8e49b67b13aac76c1f9a119bee6c4d3
ssisaias <luiz.isaias95@gmail.com>
2016-10-24 03:00:50 -0300

    Atualiza app properties adiciona service do usuario

---
+++ src/main/java/br/qxd/smartgrid/service/UsuarioService.java
@@ -0,0 +1,7 @@
+package br.qxd.smartgrid.service;
+
+import br.qxd.smartgrid.repository.UsuarioRepository;
+
+public class UsuarioService{ //implements UsuarioRepository{
+	//TODO implementar os services
+}
