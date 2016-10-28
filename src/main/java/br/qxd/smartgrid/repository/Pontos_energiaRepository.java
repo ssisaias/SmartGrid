883b449be48b1aba886751a9db1d5ed3b207c002
ssisaias <luiz.isaias95@gmail.com>
2016-10-28 16:53:57 -0300

    Add ponto de energia

---
+++ src/main/java/br/qxd/smartgrid/repository/Pontos_energiaRepository.java
@@ -0,0 +1,9 @@
+package br.qxd.smartgrid.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.qxd.smartgrid.model.Pontos_energia;
+
+public interface Pontos_energiaRepository extends JpaRepository<Pontos_energia, String> {
+
+}
