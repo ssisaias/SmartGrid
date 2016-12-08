00d40dd6ef60dc89d24d2e8b4efaf16bc69e3131
joshua-lopest <joshualopest.ce@gmail.com>
2016-12-07 21:58:29 -0300

    potenciometro adicionado e alteracoes no layout

--- src/main/java/br/qxd/smartgrid/controller/LoginController.java
+++ src/main/java/br/qxd/smartgrid/controller/LoginController.java
@@ -11,9 +11,24 @@ public class LoginController {
 	public String login(){
 		return "login/login";
 	}
-	
+	@RequestMapping(value={"/create_account"})
+	public String create_account(){
+		return "login/create_account";
+	}
 	@RequestMapping(value={"/home"})
 	public String home(){
 		return "tabs/home";
+	}
+	@RequestMapping(value={"/search"})
+	public String search(){
+		return "tabs/search";
+	}
+	@RequestMapping(value={"/notification"})
+	public String notification(){
+		return "tabs/notification";
+	}
+	@RequestMapping(value={"/account"})
+	public String account(){
+		return "tabs/account";
 	}
 }
