ac2f5ae80acca36050a66c006942b4d36c489e8a
Isaias S. (izzy) <luiz.isaias95@gmail.com>
2016-12-09 04:56:59 -0300

    Connexao com postgre

---
+++ src/main/java/br/qxd/smartgrid/AppConfig.java
@@ -0,0 +1,21 @@
+package br.qxd.smartgrid;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+
+@Configuration
+public class AppConfig {
+	
+		@Bean
+	    @Primary
+	    @ConfigurationProperties(prefix = "spring.datasource")
+		public DataSource dataSource() throws ClassNotFoundException {
+			return DataSourceBuilder.create().build();
+		}
+}
