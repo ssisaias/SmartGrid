c0fb8d58ae5f0960f30a54a6f1e5525928f817c9
Isaias S. (izzy) <luiz.isaias95@gmail.com>
2016-12-09 03:20:49 -0300

    Consulta consumo

--- src/main/java/br/qxd/smartgrid/controller/RegistroController.java
+++ src/main/java/br/qxd/smartgrid/controller/RegistroController.java
@@ -1,7 +1,14 @@
 package br.qxd.smartgrid.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +17,8 @@ import br.qxd.smartgrid.model.Registro;
 
 @Controller()
 public class RegistroController {
+	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@RequestMapping(value = { "/consumo/get","/search" }, method = RequestMethod.GET)
 	public String getConsumo() {
@@ -17,7 +26,20 @@ public class RegistroController {
 	}
 	
 	@RequestMapping(value = { "/consumo/get", }, method = RequestMethod.POST)
-	public String buscarConsumo() {
+	public String buscarConsumo(HttpServletRequest request) {
+
+		try {
+			String inicio = request.getParameter("inicio");
+			String fim = request.getParameter("fim");
+			SimpleDateFormat formatBra = new SimpleDateFormat("dd/MM/yyyy");
+
+			Date dataIni = formatBra.parse(inicio);
+			Date dataFim = formatBra.parse(fim);
+			log.info(dataIni.toString() + " " + dataFim.toString());
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		return "tabs/search";
 	}
 }
