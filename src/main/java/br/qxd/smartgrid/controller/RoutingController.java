c616c3eb27555a7da678213c9fe3033738228c56
Isaias S. (izzy) <luiz.isaias95@gmail.com>
2016-12-08 22:33:36 -0300

    Renomeia logincontroller para routingcontroller

--- src/main/java/br/qxd/smartgrid/controller/RoutingController.java
+++ src/main/java/br/qxd/smartgrid/controller/RoutingController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class LoginController {
+public class RoutingController {
 
 	
 	@RequestMapping(value={"/","/login"})
