3e3f9b92843702aae1d179d2e7ce5213ee0cea14
Isaias S. (izzy) <luiz.isaias95@gmail.com>
2016-12-15 11:34:51 -0300

    Adiciona variavel de sessao user_logged

--- src/main/java/br/qxd/smartgrid/controller/RoutingController.java
+++ src/main/java/br/qxd/smartgrid/controller/RoutingController.java
@@ -1,15 +1,32 @@
 package br.qxd.smartgrid.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import br.qxd.smartgrid.model.Usuario;
+import br.qxd.smartgrid.service.UsuarioService;
+
 @Controller
 public class RoutingController {
-
+	
+	@Autowired
+	private UsuarioService userSvc;
 	
 	@RequestMapping(value = "/login",  method = { RequestMethod.GET, RequestMethod.POST })
-	public String login(){
+	public String login(HttpServletRequest request, HttpSession session){
+		String login = request.getUserPrincipal().getName();
+		//Buscar usuario pelo login
+		Usuario usr = userSvc.findByLogin(login);
+		if (usr == null)
+			//Tratar se tiver nulo
+
+		session.setAttribute("user_logged", usr);
+
 		return "login";
 	}
 	
